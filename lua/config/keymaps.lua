b35d4fc7d188319c75f2e33e040c6d2edc9ef73e
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-30 22:45:04 +0800

    feat: keymap & options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.api.nvim_set_keymap("i", ",.", "<Esc>", {})
+vim.keymap.set({ "i", "v" }, "jk", "<Esc>", { desc = "Exit to normal mode" })
+
+vim.keymap.set({ "i", "x", "n", "s" }, "<A-s>", "<cmd>w<cr><esc>", { desc = "Save file" }) --  use Alt-s to save as well

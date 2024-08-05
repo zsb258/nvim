f04e50b32016a8b1f7a3e3af156585450a9a6b21
zsb258 <zhuang.shengbin@u.nus.edu>
2024-08-05 21:03:30 +0800

    add: keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@
 vim.keymap.set({ "i", "v" }, "jk", "<Esc>", { desc = "Exit to normal mode" })
 
 vim.keymap.set({ "i", "x", "n", "s" }, "<A-s>", "<cmd>w<cr><esc>", { desc = "Save file" }) --  use Alt-s to save as well
+
+vim.keymap.set({ "n" }, "<leader>i", "i_<Esc>r", { desc = "Insert single char" })
+vim.keymap.set({ "n" }, "<leader>a", "a_<Esc>r", { desc = "Append single char" })
